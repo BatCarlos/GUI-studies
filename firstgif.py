1d5a99f6dcbfa78da32aaebaa1c7f6240bff8ab3
BatCarlos <carlos.eduardo.ramos.duarte@gmail.com>
2024-09-11 23:06:48 -0300

    adding the first codes that display a GIF

---
+++ firstgif.py
@@ -0,0 +1,45 @@
+from tkinter import *
+from PIL import Image
+
+root = Tk()
+
+root.attributes('-fullscreen', True)
+
+root.config(background='#d9d9d9')
+
+gifImage = "Miss-ezgif.com-crop.gif"
+
+openImage = Image.open(gifImage)
+
+frames = openImage.n_frames
+
+imageObject = [PhotoImage(file= gifImage, format=f"gif -index {i}") for i in range(frames)]
+
+count = 0
+
+showAnimation = None
+
+def animation(count):
+
+    global showAnination
+
+    newImage = imageObject[count]
+
+    gif_Label.configure(image=newImage)
+    gif_Label.place(x=(root.winfo_width() - newImage.width()) // 2, y=(root.winfo_height() - newImage.height()) // 2, width=newImage.width(), height=newImage.height())
+
+    count += 1
+
+    if count == frames:
+
+        count = 0
+
+    showAnimation = root.after(40, lambda: animation(count))
+
+gif_Label = Label(root, image="")
+
+gif_Label. place(x=15, y=20, width=450, height=500)
+
+animation(count)
+
+root.mainloop()
---
+++ pygletgif.py
@@ -0,0 +1,20 @@
+# Pacote "Leitão" para display de janelas e multimídia
+import pyglet as pg
+
+file = "Miss-ezgif.com-crop.gif"
+
+animation = pg.resource.animation(file)
+sprite = pg.sprite.Sprite(animation)
+
+win = pg.window.Window(fullscreen=True)
+sprite.x = win.width // 2 - sprite.width // 2
+sprite.y = win.height // 2 - sprite.height // 2
+color = 1,1,1,1
+pg.gl.glClearColor(*color)
+
+@win.event
+def on_draw():
+    win.clear()
+    sprite.draw()
+
+pg.app.run()
